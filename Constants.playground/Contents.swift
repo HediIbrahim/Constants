3937d434476b8439bedab89c79fc7bf252a3ed3b
Hedi <HediIbrahim@users.noreply.github.com>
2017-04-14 12:05:33 -0400

    Add files via upload

---
+++ Constants.playground/Contents.swift
@@ -0,0 +1,20 @@
+var favoriteColor = "Cyan"
+
+favoriteColor = "Light pink"
+
+print(favoriteColor)
+
+
+let momsName = "Susan"
+
+//momsName = "Patty"
+
+let friendsName = "Diana"
+
+let anotherFriendsName = "Sara"
+
+let favoriteMovie = "Boss Baby"
+
+
+
+		
